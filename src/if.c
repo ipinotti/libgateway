b2c0f1f9d09a004426c096e07325ee94fd2a16a7
Igor Kramer Pinotti <ipinotti@pd3.com.br>
2012-08-23 14:57:05 -0300

    add gateway default hwaddr get

--- src/if.c
+++ src/if.c
@@ -26,8 +26,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
-#include <syslog.h>
+#include <fcntl.h>
+#include <ctype.h>
 
 #include <sys/ioctl.h>
 #include <sys/socket.h>
@@ -36,9 +36,16 @@
 #include <netinet/in.h>
 #include <ifaddrs.h>
 
+#include <net/if.h>
+#include <netinet/ip_icmp.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #include <asterisk.h>
 #include <asterisk/module.h>
 #include <asterisk/logger.h>
+#include <if.h>
 
 #include "libnetlink/libnetlink.h"
 
@@ -138,6 +145,80 @@ static int _filter_default_gw(const struct sockaddr_nl *who, struct nlmsghdr *n,
 	return 0;
 }
 
+static int _dev_get_ctrlfd(void)
+{
+	int s_errno;
+	int fd;
+
+	fd = socket(PF_INET, SOCK_DGRAM, 0);
+	if (fd >= 0)
+		return fd;
+
+	s_errno = errno;
+
+	fd = socket(PF_PACKET, SOCK_DGRAM, 0);
+	if (fd >= 0)
+		return fd;
+
+	fd = socket(PF_INET6, SOCK_DGRAM, 0);
+	if (fd >= 0)
+		return fd;
+
+	errno = s_errno;
+	perror("Cannot create control socket");
+
+	return -1;
+}
+
+static int _in_cksum(unsigned short *buf, int sz)
+{
+
+	int nleft = sz;
+	int sum = 0;
+	unsigned short *w = buf;
+	unsigned short ans = 0;
+
+	while (nleft > 1) {
+		sum += *w++;
+		nleft -= 2;
+	}
+
+	if (nleft == 1) {
+		*(unsigned char *) (&ans) = *(unsigned char *) w;
+		sum += ans;
+	}
+
+	sum = (sum >> 16) + (sum & 0xFFFF);
+	sum += (sum >> 16);
+	ans = ~sum;
+	return ans;
+}
+
+static int _dev_get_flags(char *dev, __u32 *flags)
+{
+	struct ifreq ifr;
+	int fd;
+	int err;
+
+	strcpy(ifr.ifr_name, dev);
+	fd = _dev_get_ctrlfd();
+
+	if (fd < 0)
+		return -1;
+
+	err = ioctl(fd, SIOCGIFFLAGS, &ifr);
+	if (err) {
+		//pr_error(1, "dev_get_flags %s SIOCGIFFLAGS", ifr.ifr_name);
+		close(fd);
+		return -1;
+	}
+
+	*flags = ifr.ifr_flags;
+	close(fd);
+
+	return err;
+}
+
 int libamg_get_default_gw_ipaddr(struct in_addr *ip)
 {
 	struct rtnl_handle rth;
@@ -164,31 +245,182 @@ rtnl_err:
 	return errno;
 }
 
+int libamg_dev_get_link_running(char *dev)
+{
+	__u32 flags;
 
-int libamg_get_default_gw_hwaddr(char *addr)
+	if (_dev_get_flags(dev, &flags) < 0)
+		return (-1);
+
+	return (flags & IFF_RUNNING);
+}
+
+int libamg_ping(char *ipaddr, char *intf)
+{
+	enum {
+		DEFDATALEN = 56,
+		MAXIPLEN = 60,
+		MAXICMPLEN = 76,
+		MAXPACKET = 65468,
+		MAX_DUP_CHK = (8 * 128),
+		MAXWAIT = 10,
+		PINGINTERVAL = 1,
+	/* 1 second */
+	};
+
+	struct sockaddr_in pingaddr;
+	struct icmp *pkt;
+	int pingsock, c, i, ret = 0;
+	long arg;
+	char packet[DEFDATALEN + MAXIPLEN + MAXICMPLEN];
+	struct ifreq ifr;
+
+	if_dbg("Pinging %s ... \n", ipaddr);
+
+	if (libamg_dev_get_link_running(intf) <= 0)
+		return -1;
+
+	pingsock = socket(AF_INET, SOCK_RAW, 1); /* 1 == ICMP */
+	pingaddr.sin_family = AF_INET;
+	pingaddr.sin_addr.s_addr = inet_addr(ipaddr);
+
+	/* Force source address to be of the interface we want */
+	ifr.ifr_addr.sa_family = AF_INET;
+	strncpy(ifr.ifr_name, intf, IFNAMSIZ - 1);
+	ioctl(pingsock, SIOCGIFADDR, &ifr);
+
+	if_dbg("Ping interface %s. IP is %s\n", intf,
+			inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+
+	if (bind(pingsock, (struct sockaddr*) &ifr.ifr_addr, sizeof(struct sockaddr_in)) == -1) {
+		perror("bind");
+		exit(2);
+	}
+
+	pkt = (struct icmp *) packet;
+	memset(pkt, 0, sizeof(packet));
+
+	pkt->icmp_type = ICMP_ECHO;
+	pkt->icmp_cksum = _in_cksum((unsigned short *) pkt, sizeof(packet));
+
+	c = sendto(pingsock, packet, DEFDATALEN + ICMP_MINLEN, 0, (struct sockaddr *) &pingaddr,
+	                sizeof(pingaddr));
+
+	/* Set non-blocking */
+	if ((arg = fcntl(pingsock, F_GETFL, NULL)) < 0) {
+		fprintf(stderr, "Error fcntl(..., F_GETFL) (%s)\n", strerror(errno));
+		return -1;
+	}
+
+	arg |= O_NONBLOCK;
+
+	if (fcntl(pingsock, F_SETFL, arg) < 0) {
+		fprintf(stderr, "Error fcntl(..., F_SETFL) (%s)\n", strerror(errno));
+		return -1;
+	}
+
+	sleep(1);
+
+	/* listen for replies */
+	i = 30; /* Number of attempts */
+	while (i--) {
+		struct sockaddr_in from;
+		socklen_t fromlen = sizeof(from);
+
+		c = recvfrom(pingsock, packet, sizeof(packet), 0, (struct sockaddr *) &from,
+		                &fromlen);
+
+		if_dbg("recvfrom returned %d bytes\n", c);
+
+		if (c < 0) {
+			usleep(10000);
+			continue;
+		}
+
+		if (c >= 76) { /* ip + icmp */
+			struct iphdr *iphdr = (struct iphdr *) packet;
+
+			pkt = (struct icmp *) (packet + (iphdr->ihl << 2)); /* skip ip hdr */
+			if (pkt->icmp_type == ICMP_ECHOREPLY) {
+				ret = 1;
+				break;
+			}
+		}
+	}
+
+	close(pingsock);
+	return ret;
+}
+
+int libamg_arp_resolv(struct in_addr *ipaddr)
+{
+	char ipaddr_str[16];
+	memset(ipaddr_str, 0, sizeof(ipaddr_str));
+	inet_ntop(AF_INET, (const void *)ipaddr, ipaddr_str, sizeof(ipaddr_str));
+
+	return libamg_ping(ipaddr_str, MAIN_IF);
+}
+
+int libamg_arp_get_hwaddr(struct in_addr *ipaddr, char *hwaddr)
+{
+	char line [128];
+	char ipaddr_str[16];
+	char hwaddr_str[18];
+	FILE * file;
+
+	memset(line, 0, sizeof(line));
+	memset(ipaddr_str, 0, sizeof(ipaddr_str));
+	memset(hwaddr_str, 0, sizeof(hwaddr_str));
+	inet_ntop(AF_INET, (const void *)ipaddr, ipaddr_str, sizeof(ipaddr_str));
+
+	if ((file = fopen(ARP_ENTRIES_FILE, "r")) == NULL) {
+		syslog(LOG_ERR, "Could not open ARP Entries File at [%s]\n", ARP_ENTRIES_FILE);
+		return -1;
+	}
+
+	while (fgets(line, sizeof(line), file) != NULL) {
+		if (strstr(line, ipaddr_str)){
+			if (sscanf(line, "%*s %*s %*s %s", hwaddr_str) < 1){
+				fclose(file);
+				return -1;
+			}
+		}
+	}
+
+	fclose(file);
+
+	sscanf(hwaddr_str, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+			&hwaddr[0],
+			&hwaddr[1],
+			&hwaddr[2],
+			&hwaddr[3],
+			&hwaddr[4],
+			&hwaddr[5]);
+
+	return 0;
+}
+
+int libamg_get_default_gw_hwaddr(char *hwaddr)
 {
 	struct in_addr gw_ip_addr;
 
-	memset(addr, 0, 6);
+	memset(&gw_ip_addr, 0, sizeof(gw_ip_addr));
+	memset(hwaddr, 0, 6);
 
-	/* TODO STEP 1: Get default gateway IP */
+	/* STEP 1: Get default gateway IP */
 	if (libamg_get_default_gw_ipaddr(&gw_ip_addr) < 0)
 		return -1;
 
-	/* TODO STEP 2: Do ARP on that IP */
-	/* XXX Force resolution of hardware address
-	if (libamg_arp_resolv(ipaddr) < 0)
+	/* STEP 2: Do ARP on that IP */
+	/* Force resolution of hardware address */
+	if (libamg_arp_resolv(&gw_ip_addr) < 0)
 		return -1;
-	 */
 
-	/* TODO STEP 3: Check ARP table for HW address */
-	/* XXX
-	if (libamg_get_peer_hwaddr(ipaddr, hwaddr) < 0) {
-		syslog(LOG_ERROR, "Could not get default gateway's MAC address\n");
+	/* STEP 3: Check ARP table for HW address */
+	if (libamg_arp_get_hwaddr(&gw_ip_addr, hwaddr) < 0) {
+		syslog(LOG_ERR, "Could not get default gateway's MAC address\n");
 		return -1;
 	}
-	*/
-
 
 	return 0;
 }
--- src/if.h
+++ src/if.h
@@ -1,4 +1,4 @@
-/*
+/*to
  * if.h
  *
  *  Created on: Jul 12, 2012
@@ -7,6 +7,38 @@
 
 #ifndef IF_H_
 #define IF_H_
+
+#include <errno.h>
+#include <syslog.h>
+
+/*
+ * Debug Function
+ */
+#if defined (DEBUG_IF_SYSLOG)
+#define if_dbg(x,...) \
+		syslog(LOG_INFO,  "%s : %d => "x, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#elif defined (DEBUG_IF_PRINTF)
+#define if_dbg(x,...) \
+		printf("%s : %d => "x, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define if_dbg(x,...)
+#endif
+
+/*
+ * General Defines
+ */
+#define MAIN_IF "eth0"
+#define ARP_ENTRIES_FILE "/proc/net/arp"
+
+/*
+ * General Structures
+ */
+
+
+
+/*
+ * Functions Declaration
+ */
 
 /**
  * libamg__get_default_gw_ipaddr
@@ -42,7 +74,7 @@ int libamg_get_interface_hwaddr(char *iface, char *addr);
  *
  * @param iface
  * @param addr
- * @return
+ * @return 0 if success, -1 if error
  */
 int libamg_get_interface_ipv4_addr(char *iface,  struct in_addr *addr);
 
@@ -51,9 +83,53 @@ int libamg_get_interface_ipv4_addr(char *iface,  struct in_addr *addr);
  *
  * @param iface
  * @param addr
- * @return
+ * @return 0 if success, -1 if error
  */
 int libamg_get_interface_ipv6_addr(char *iface, struct in6_addr *addr);
+
+
+/**
+ * libamg_arp_get_hwaddr
+ *
+ * Get HW address related to IPv4 address in ARPs table
+ *
+ * @param ip_addr
+ * @param hwaddr
+ * @return 0 if success, -1 if error
+ */
+int libamg_arp_get_hwaddr(struct in_addr *ipaddr, char *hwaddr);
+
+/**
+ * libamg_arp_resolv
+ *
+ * Executes a ping targeting the provided ipaddr on the default interface
+ * to fulfill the ARP table
+ *
+ * @param ipaddr
+ * @return 0 if success, -1 if error
+ */
+int libamg_arp_resolv(struct in_addr *ipaddr);
+
+/**
+ * libamg_ping
+ *
+ * Executes a ping targeting the provided ipaddr on the intf (interface)
+ *
+ * @param ipaddr
+ * @param intf
+ * @return 0 if success, -1 if error
+ */
+int libamg_ping(char *ipaddr, char *intf);
+
+/**
+ * libamg_dev_get_link_running
+ *
+ * Return the status about link running of a given dev (interface)
+ *
+ * @param dev
+ * @return 0 if not running, > 0 if running
+ */
+int libamg_dev_get_link_running(char *dev);
 
 
 #endif /* IF_H_ */
