3471cf1ebf6cc4d02f2098f5167dd5055a6f5122
Igor Kramer Pinotti <ipinotti@pd3.com.br>
2012-12-27 18:23:14 -0200

    add sys_log features: enable_remote, loglevel, remote host, apply conf, reset conf.

--- src/sys_log.c
+++ src/sys_log.c
@@ -22,4 +22,125 @@
  *
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
 
+#include "sys_log.h"
+#include "log.h"
+#include "str.h"
+
+#define BUF_SIZE	256
+
+int libamg_sys_log_daemon_apply_config(void)
+{
+	struct libamg_sys_log_config *config = libamg_sys_log_parse_config();
+	char command[BUF_SIZE];
+	int length = 0;
+
+
+	system("/bin/pkill syslogd");
+	memset(command, 0, sizeof(command));
+	length += sprintf(command, "%s", DAEMON_SYS_LOG);
+
+	if (config->remote_enable){
+		if (config->loglevel)
+			length += sprintf(command+length, " -l %d", config->loglevel);
+		if (strlen(config->remote_host) > 0)
+			length += sprintf(command+length, " -L -R %s:%d", config->remote_host, DAEMON_PORT_SYS_LOG);
+	}
+
+	system(command);
+
+	free(config);
+
+	return 0;
+}
+
+int libamg_sys_log_reset_config(void)
+{
+	char command[BUF_SIZE];
+
+	memset(command, 0, sizeof(command));
+	snprintf(command, sizeof(command), "cp %s%s %s", FILE_SYS_LOG_CONF_DEFAULT_PATH, FILE_SYS_LOG_CONF_NAME, FILE_SYS_LOG_CONF_PATH);
+
+	return system(command);
+}
+
+struct libamg_sys_log_config *libamg_sys_log_parse_config(void)
+{
+	FILE *file;
+	char buffer[BUF_SIZE];
+	char *key;
+	char *value;
+	struct libamg_sys_log_config *conf;
+
+	/* Alloc config struct */
+	conf = malloc(sizeof(struct libamg_sys_log_config));
+	if (!conf) {
+		libamg_log_error("Error allocating memory\n")
+		return NULL;
+	}
+	memset(conf, 0, sizeof(struct libamg_sys_log_config));
+
+	/* Open IP config file */
+	file = fopen(FILE_SYS_LOG_CONF, "r");
+	if (file == NULL) {
+		libamg_log_error("Error opening file\n");
+		return NULL;
+	}
+
+	/* Load sys_log config and add CGI parameters */
+	while (fgets(buffer, BUF_SIZE, file)) {
+		/* Parse key and value */
+		key = buffer;
+		if (strchr(key, '=') == NULL)
+			continue;
+		value = libamg_str_next_token(buffer, '=');
+
+		/* Crop trailing commentary */
+		strtok(value, " \t\n\0;#");
+
+		/* Parse parameters */
+			/* General confs */
+		if (!strcmp(key, "remote_enable")) {
+			conf->remote_enable = atoi(value);
+		} else if (!strcmp(key, "loglevel")) {
+			conf->loglevel = atoi(value);
+		} else if (!strcmp(key, "remote_host")) {
+			strncpy(conf->remote_host, value, 63);
+		}
+	}
+	fclose(file);
+
+	return conf;
+}
+
+int libamg_sys_log_save_config(struct libamg_sys_log_config *conf)
+{
+	FILE *file;
+
+	/* Open sys_log config file */
+	file = fopen(FILE_SYS_LOG_CONF, "w");
+	if (file == NULL) {
+		libamg_log_error("Error opening file\n");
+		return -1;
+	}
+
+	fprintf(file, "remote_enable=%d\n", conf->remote_enable);
+
+	fprintf(file, "loglevel=%d\n", conf->loglevel);
+
+	fprintf(file, "remote_host=%s\n", conf->remote_host);
+
+	fclose(file);
+
+	return 0;
+}
--- src/sys_log.h
+++ src/sys_log.h
@@ -26,5 +26,77 @@
 #define SYS_LOG_H_
 
 
+/*
+ * General Includes
+ */
+
+
+
+/*
+ * General Defines
+ */
+
+#define FILE_SYS_LOG_CONF_NAME			"sys_log.conf"
+#define FILE_SYS_LOG_CONF_PATH			"/etc/"
+#define FILE_SYS_LOG_CONF				FILE_SYS_LOG_CONF_PATH FILE_SYS_LOG_CONF_NAME
+#define FILE_SYS_LOG_CONF_DEFAULT_PATH	"/etc.ro/"
+
+#define DAEMON_SYS_LOG		"/bin/syslogd"
+
+#define DAEMON_PORT_SYS_LOG 172
+
+
+/*
+ * General Structures
+ */
+
+struct libamg_sys_log_config {
+	int remote_enable;
+	int loglevel;
+	char remote_host[64];
+};
+
+
+/*
+ * Functions Declaration
+ */
+
+/**
+ * libamg_sys_log_daemon_apply_config
+ *
+ * Apply syslog daemon configs.
+ *
+ * @return 0 if ok
+ */
+int libamg_sys_log_daemon_apply_config(void);
+
+/**
+ * libamg_sys_log_reset_config
+ *
+ * Restore the sys_log configures from default sys_log configuration file
+ *
+ * @return 0 if success, negative if error
+ */
+int libamg_sys_log_reset_config(void);
+
+/**
+ * libamg_sys_log_parse_config
+ *
+ * Get sys_log configs through libamg_sys_log_config structure.
+ *
+ * @return struct libamg_sys_log_config *
+ */
+struct libamg_sys_log_config *libamg_sys_log_parse_config(void);
+
+/**
+ * libamg_sys_log_save_config
+ *
+ * Saves sys_log configs at libamg_sys_log_config structure
+ * inside sys_log configuration file.
+ *
+ * @param conf
+ * @return -1 if error, 0 if OK
+ */
+int libamg_sys_log_save_config(struct libamg_sys_log_config *conf);
 
 #endif /* SYS_LOG_H_ */
